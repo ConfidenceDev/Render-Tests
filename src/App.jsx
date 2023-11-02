eefbc91e0788fc39d2a2e4c4599a52f7ecd3c429
Dev-Geek <confidostic3@gmail.com>
2023-11-02 21:31:39 +0100

    Trial 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import { Login, Room } from "./components";
-// import { ToastContainer } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css";
-// import { Provider } from "react-redux";
-// import store from "./store";
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Login, Room } from "./components"
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+//import { Provider } from "react-redux"
+//import store from "./store";
 
 export default function App() {
   return (
     <>
-      <div>
-        <h1 color="white">HELLO WORLD</h1>
-      </div>
-
       {/* <Provider store={store}>
-        <BrowserRouter>
-          <ToastContainer className="toast" />
-          <Routes>
-            <Route path="/lecture/:room" element={<Login />} />
-            <Route path="/lecture/:room/:access" element={<Room />} />
-          </Routes>
-        </BrowserRouter>
-      </Provider> */}
+        
+      </Provider>  */}
+      <BrowserRouter>
+        <ToastContainer className="toast" />
+        <Routes>
+          <Route path="/lecture/:room" element={<Login />} />
+          <Route path="/lecture/:room/:access" element={<Room />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }

4de0245a7f4c3af2489384b787c5aa61c2eb420c
Dev-Geek <confidostic3@gmail.com>
2023-11-02 21:36:30 +0100

    Trial 4

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,24 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Login, Room } from "./components"
 import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
-//import { Provider } from "react-redux"
-//import store from "./store";
+import { Provider } from "react-redux"
+import store from "./store"
 
 export default function App() {
   return (
     <>
-      {/* <Provider store={store}>
-        
-      </Provider>  */}
-      <BrowserRouter>
+      <Provider store={store}>
+        {/* <BrowserRouter> */}
         <ToastContainer className="toast" />
-        <Routes>
-          <Route path="/lecture/:room" element={<Login />} />
-          <Route path="/lecture/:room/:access" element={<Room />} />
-        </Routes>
-      </BrowserRouter>
+        <div>
+          <h1>HELLO WORLD!!!</h1>
+        </div>
+        {/* <Routes>
+            <Route path="/lecture/:room" element={<Login />} />
+            <Route path="/lecture/:room/:access" element={<Room />} />
+          </Routes>
+        </BrowserRouter> */}
+      </Provider>
     </>
   )
 }
